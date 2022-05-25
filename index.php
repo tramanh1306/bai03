9a7441644159450ad3a08be0435c019161d05628
tramanh1306 <100921382+tramanh1306@users.noreply.github.com>
2022-05-25 13:48:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+	$a=5;	
+	echo $a;
+?><!--1p17-->
+<br>
+<?php
+	$a3 = 	5;
+	echo $a3;
+?><!--2p15-->
+<br>
+<?php
+	$a = 5;
+	$A = 10;
+	echo $a;
+	echo '<br>';
+	echo $A
+?><!--3p39-->
+<br>
+<?php
+	define('PI', 3.14);
+	echo PI;
+?><!--5p48-->
+<br>
+<?php
+	$str = "Hello World";
+	echo $str;
+?><!--6p58-->
+<br>
+<?php
+	$firstName = 'Le Ha';
+	$lastName = 'Quoc Thinh';
+	$fullName = $firstName . ' '. $lastName;
+	echo $fullName;
+?><!--8p25-->
